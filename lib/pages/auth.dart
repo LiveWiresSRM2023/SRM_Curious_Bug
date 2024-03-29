657132ba2c24ed568d93c0d840c7953cb12d4b8f
dev-Roshan-lab <dev.vividgoat@gmail.com>
2024-03-29 21:40:15 +0530

    fix:update_auth_shadows

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -17,95 +17,97 @@ class _AuthState extends State<Auth> {
         // Wrap with Center widget
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Container(
-            padding: const EdgeInsets.all(30.0),
-            decoration: BoxDecoration(
-              boxShadow: const [
-                BoxShadow(
-                  color: Color(0xFFA8CBF6),
-                  offset: Offset(0, 0),
-                  blurRadius: 30,
-                  spreadRadius: 7.0,
-                )
-              ],
-              color: const Color.fromARGB(255, 255, 255, 255),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            height: 600,
-            width: 500,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/srm_logo.png',
-                  width: 200,
-                  height: 200,
-                ),
-                Column(
-                  children: [
-                    Text(
-                      'Explore, share and ignite your new research',
-                      style: GoogleFonts.inter(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFFA8CBF6),
+                    offset: Offset(0, 0),
+                    blurRadius: 30,
+                    spreadRadius: 7.0,
+                  )
+                ],
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 600,
+              width: 500,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/srm_logo.png',
+                    width: 200,
+                    height: 200,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Explore, share and ignite your new research',
+                        style: GoogleFonts.inter(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      '"Dive into the Depths of Discovery: Unleash the Power of Research at your Fingertips"',
-                      style: GoogleFonts.inter(fontSize: 14),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      'Sign In with Google',
-                      style: GoogleFonts.inter(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+                      const SizedBox(height: 20),
+                      Text(
+                        '"Dive into the Depths of Discovery: Unleash the Power of Research at your Fingertips"',
+                        style: GoogleFonts.inter(fontSize: 14),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      width: 200,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF0C4DA2),
-                        borderRadius: BorderRadius.circular(20),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Sign In with Google',
+                        style: GoogleFonts.inter(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      child: InkWell(
-                        onTap: () {},
-                        splashColor: Colors.white.withOpacity(0.5),
-                        highlightColor: Colors.transparent,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/google-icon.png',
-                              width: 24,
-                              height: 24,
-                            ),
-                            const SizedBox(
-                                width: 8), // Add space between text and image
-                            Text(
-                              'Google',
-                              style: GoogleFonts.inter(
-                                color: Colors.white,
-                                fontSize: 18,
+                      const SizedBox(height: 20),
+                      Container(
+                        width: 200,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF0C4DA2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          splashColor: Colors.white.withOpacity(0.5),
+                          highlightColor: Colors.transparent,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/images/google-icon.png',
+                                width: 24,
+                                height: 24,
                               ),
-                            ),
-                          ],
+                              const SizedBox(
+                                  width: 8), // Add space between text and image
+                              Text(
+                                'Google',
+                                style: GoogleFonts.inter(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
