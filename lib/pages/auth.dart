e63db606606d0cd684b7f497d03e81060ac31c0a
vaseem15905 <138885639+vaseem15905@users.noreply.github.com>
2024-03-29 19:23:54 +0530

    auth_v1

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,17 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Auth extends StatefulWidget {
-  const Auth({super.key});
+  const Auth({Key? key}) : super(key: key);
 
   @override
   State<Auth> createState() => _AuthState();
 }
 
 class _AuthState extends State<Auth> {
-  // TODO: Implement firebase auth
-  // TODO: Implement Auth Flow
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        // Wrap with Center widget
+        child: Container(
+          padding: EdgeInsets.all(30.0),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFFA8CBF6),
+                offset: Offset(0, 0),
+                blurRadius: 30,
+                spreadRadius: 7.0,
+              )
+            ],
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          height: 600,
+          width: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/srm_logo.png',
+                width: 200,
+                height: 200,
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Explore, share and ignite your new research',
+                    style: GoogleFonts.archivo(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    '"Dive into the Depths of Discovery: Unleash the Power of Research at your Fingertips"',
+                    style: GoogleFonts.inter(fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Sign In with Google',
+                    style: GoogleFonts.archivo(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF0C4DA2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      splashColor: Colors.white.withOpacity(0.5),
+                      highlightColor: Colors.transparent,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/google-icon.png',
+                            width: 24,
+                            height: 24,
+                          ),
+                          SizedBox(
+                              width: 8), // Add space between text and image
+                          Text(
+                            'Google',
+                            style: GoogleFonts.inter(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Auth(),
+  ));
 }
