586958645cb7e111e0e5991602422fff671d46da
pxnchxm1 <panchamipkumar1995@gmail.com>
2024-03-30 12:46:34 +0530

    fix:profilepage_sizedboxheight

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -12,91 +12,94 @@ class Profile extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Center(
         // Wrap with Center widget
-        child: Container(
-          padding: const EdgeInsets.all(30.0),
-          decoration: BoxDecoration(
-            boxShadow: const [
-              BoxShadow(
-                color: Color(0xFFA8CBF6),
-                offset: Offset(0, 0),
-                blurRadius: 30,
-                spreadRadius: 7.0,
-              )
-            ],
-            color: const Color.fromARGB(255, 255, 255, 255),
-            borderRadius: BorderRadius.circular(
-                20), // Corrected border radius value // Set border color to blue
-          ),
-          height: screenSize.height * 0.85, // 80% of the screen height
-          width: screenSize.width * 0.3, // 80% of the screen width
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Edit your Profile',
-                style: GoogleFonts.inter(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            padding: const EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                  color: Color(0xFFA8CBF6),
+                  offset: Offset(0, 0),
+                  blurRadius: 30,
+                  spreadRadius: 7.0,
+                )
+              ],
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(
+                  20), // Corrected border radius value // Set border color to blue
+            ),
+            height: screenSize.height * 0.9, // 80% of the screen height
+            width: screenSize.width * 0.3, // 80% of the screen width
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Edit your Profile',
+                  style: GoogleFonts.inter(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Image.asset(
-                'assets/images/srm_logo.png',
-                width: 80,
-                height: 80,
-              ),
-              SizedBox(height: 10),
-              ProfileTextField(labelText: 'Name'),
-              ProfileTextField(labelText: 'College'),
-              ProfileTextField(labelText: 'Position in the department'),
-              ProfileTextField(labelText: 'Social Links'),
-              SizedBox(height: 12),
-              Column(
-                children: [
-                  Text(
-                    'Sign In with Google',
-                    style: GoogleFonts.inter(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 20),
-                  InkWell(
-                    onTap: () {},
-                    splashColor: Colors.white.withOpacity(0.5),
-                    highlightColor: Colors.transparent,
-                    child: Container(
-                      width: 150,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF0C4DA2),
-                        borderRadius: BorderRadius.circular(20),
+                const SizedBox(height: 5),
+                Image.asset(
+                  'assets/images/srm_logo.png',
+                  width: 80,
+                  height: 80,
+                ),
+                const SizedBox(height: 5),
+                const ProfileTextField(labelText: 'Name'),
+                const ProfileTextField(labelText: 'College'),
+                const ProfileTextField(labelText: 'Position in the department'),
+                const ProfileTextField(labelText: 'Social Links'),
+                const SizedBox(height: 5),
+                Column(
+                  children: [
+                    Text(
+                      'Sign In with Google',
+                      style: GoogleFonts.inter(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Continue',
-                            style: GoogleFonts.inter(
-                              color: Colors.white,
-                              fontSize: 14,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    InkWell(
+                      onTap: () {},
+                      splashColor: Colors.white.withOpacity(0.5),
+                      highlightColor: Colors.transparent,
+                      child: Container(
+                        width: 150,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF0C4DA2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Continue',
+                              style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
                             ),
-                          ),
-                          const SizedBox(
-                              width: 8), // Add space between text and image
-                          Icon(
-                            Icons.arrow_forward_rounded,
-                            color: Colors.white,
-                          ),
-                        ],
+                            const SizedBox(
+                                width: 8), // Add space between text and image
+                            const Icon(
+                              Icons.arrow_forward_rounded,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -115,7 +118,7 @@ class ProfileTextField extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(labelText),
-        SizedBox(height: 10),
+        const SizedBox(height: 5),
         SizedBox(
           height: 30, // Reduced height
           width: 300,
@@ -125,25 +128,20 @@ class ProfileTextField extends StatelessWidget {
                   const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(15.0),
-                borderSide: BorderSide(color: Colors.transparent),
+                borderSide: const BorderSide(color: Colors.transparent),
               ),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(15.0),
-                borderSide: BorderSide(color: Color.fromARGB(0, 197, 46, 46)),
+                borderSide:
+                    const BorderSide(color: Color.fromARGB(0, 197, 46, 46)),
               ),
               fillColor: Colors.grey[200],
               filled: true,
             ),
           ),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 10),
       ],
     );
   }
 }
-
-// void main() {
-//   runApp(MaterialApp(
-//     home: Profile(),
-//   ));
-// }
