b1590c37c6c8fa523f9e039d6d08a7fa1a69b44d
dev-Roshan-lab <63838060+dev-Roshan-lab@users.noreply.github.com>
2024-05-28 07:36:30 +0000

    Build: (2dcb1fe) feat: make post_page responsive

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2632799872"
+    serviceWorkerVersion: "1231598594"
   }
 });
