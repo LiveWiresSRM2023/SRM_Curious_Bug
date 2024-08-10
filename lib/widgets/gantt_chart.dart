38a2b3546f8d6fa6cceaa1023392d887e99c439b
dev-Roshan-lab <dev.vividgoat@gmail.com>
2024-08-10 17:09:41 +0530

    feat: gantt dialog

--- lib/widgets/gantt_chart.dart
+++ lib/widgets/gantt_chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class GanttChart extends StatelessWidget {
   final List tasks; // list of task and its specficiations
@@ -34,6 +35,8 @@ class GanttChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ScrollController controller = ScrollController();
+
     return tasks.isEmpty
         ? Center(
             child: Text(
@@ -45,115 +48,217 @@ class GanttChart extends StatelessWidget {
                   color: Theme.of(context).colorScheme.primary),
             ),
           )
-        : SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: SizedBox(
-              height: 300,
-              width: (dates.length * 50) + 20,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: List.generate(
-                          dates.length,
-                          (index) => Padding(
-                                padding: const EdgeInsets.all(5),
-                                child: Container(
-                                  height: 40,
-                                  width: 40,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    border: Border.all(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primary,
-                                        style: BorderStyle.solid),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        getDayOfTheWeek(dates[index]),
-                                        style: TextStyle(
-                                            fontSize: 13,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .primary),
-                                      ),
-                                      Text(
-                                        dates[index].day.toString(),
-                                        style: TextStyle(
-                                            fontSize: 11,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .primary,
-                                            fontWeight: FontWeight.w600),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )),
-                    ),
-                    SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                        itemCount: tasks.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          int barWidth = DateTime.parse(tasks[index]['endDate'])
-                                  .difference(DateTime.parse(
-                                      tasks[index]['currentDate']))
-                                  .inDays
-                                  .abs() +
-                              1;
-                          int paddingSize = startDate
-                              .difference(
-                                  DateTime.parse(tasks[index]['currentDate']))
-                              .inDays
-                              .abs();
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 4.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: paddingSize * 50,
-                                ),
-                                Container(
-                                  width: barWidth * 50,
-                                  height: 30,
-                                  decoration: BoxDecoration(
+        : RawScrollbar(
+            controller: controller,
+            thumbColor: Colors.black,
+            thickness: 7,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            thumbVisibility: true,
+            trackVisibility: true,
+            child: SingleChildScrollView(
+              controller: controller,
+              scrollDirection: Axis.horizontal,
+              child: SizedBox(
+                height: 300,
+                width: (dates.length * 50) + 20,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: List.generate(
+                            dates.length,
+                            (index) => Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(10),
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primary),
-                                  alignment: Alignment.centerLeft,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 8.0),
-                                    child: Text(
-                                      tasks[index]['taskName'],
-                                      textAlign: TextAlign.left,
-                                      style: const TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w600),
+                                      border: Border.all(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary,
+                                          style: BorderStyle.solid),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          getDayOfTheWeek(dates[index]),
+                                          style: TextStyle(
+                                              fontSize: 13,
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .primary),
+                                        ),
+                                        Text(
+                                          dates[index].day.toString(),
+                                          style: TextStyle(
+                                              fontSize: 11,
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .primary,
+                                              fontWeight: FontWeight.w600),
+                                        )
+                                      ],
                                     ),
                                   ),
-                                )
-                              ],
-                            ),
-                          );
-                        },
+                                )),
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        height: 200,
+                        child: ListView.builder(
+                          itemCount: tasks.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            int barWidth =
+                                DateTime.parse(tasks[index]['endDate'])
+                                        .difference(DateTime.parse(
+                                            tasks[index]['currentDate']))
+                                        .inDays
+                                        .abs() +
+                                    1;
+                            int paddingSize = startDate
+                                .difference(
+                                    DateTime.parse(tasks[index]['currentDate']))
+                                .inDays
+                                .abs();
+                            return InkWell(
+                              onTap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                          backgroundColor: Colors.white,
+                                          title: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                tasks[index]['taskName'],
+                                                textAlign: TextAlign.left,
+                                                style: GoogleFonts.inter(
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .primary,
+                                                    fontSize: 20,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              IconButton(
+                                                  onPressed: () =>
+                                                      Navigator.pop(context),
+                                                  icon: Icon(Icons.close,
+                                                      color: Theme.of(context)
+                                                          .colorScheme
+                                                          .primary))
+                                            ],
+                                          ),
+                                          content: SizedBox(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.2,
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Text(
+                                                  tasks[index]["subTasks"],
+                                                  style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 17),
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceAround,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.center,
+                                                  children: [
+                                                    Text(
+                                                      "Start Date:\n${DateFormat("EEE, M/d/y").format(DateTime.parse(tasks[index]["currentDate"]))}",
+                                                      style: TextStyle(
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .colorScheme
+                                                                  .primary,
+                                                          fontSize: 15),
+                                                    ),
+                                                    const Divider(
+                                                      color: Colors.black,
+                                                    ),
+                                                    Text(
+                                                      "End Date:\n${DateFormat("EEE, M/d/y").format(DateTime.parse(tasks[index]["endDate"]))}",
+                                                      style: TextStyle(
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .colorScheme
+                                                                  .primary,
+                                                          fontSize: 15),
+                                                    ),
+                                                  ],
+                                                )
+                                              ],
+                                            ),
+                                          ));
+                                    });
+                              },
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      width: paddingSize * 50,
+                                    ),
+                                    Container(
+                                      width: barWidth * 50,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary),
+                                      alignment: Alignment.centerLeft,
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 8.0),
+                                        child: Text(
+                                          tasks[index]['taskName'],
+                                          textAlign: TextAlign.left,
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
