76a6445276acdda750c38d63e832afcde23dc5ff
pxnchxm1 <117977315+pxnchxm1@users.noreply.github.com>
2024-08-14 16:03:33 +0000

    Build: (81a9201) feat:tabbar for events

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3180745034"
+    serviceWorkerVersion: "2391383322"
   }
 });
